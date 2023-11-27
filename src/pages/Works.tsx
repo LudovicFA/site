00e922182ceb2e6c8d30ed87005c8db2d88e9473
Ludovic FAVRE ARTIGUES <ludovic.favreartigues@gmail.com>
2023-11-27 10:38:24 +0100

    Feat: Page works en construction

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -3,6 +3,10 @@ const Works = () => {
   return (
     <section className='max-container'>
         <h1 className='head-text'>Works</h1>
+
+        <p className="bg-red-100 border-4 border-red-500 text-red-700 p-4 text-center mt-8">
+          WORK IN PROGRESS
+        </p>
     </section>
   )
 }
